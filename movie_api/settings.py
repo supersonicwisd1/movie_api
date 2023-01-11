824ede2c81bfbdf272f182a0a5d39e66ad8141cd
Orjiene Kenechukwu <supersonicwisdom@gmail.com>
2023-01-11 04:21:57 +0100

    Update settings.py

--- movie_api/settings.py
+++ movie_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5&(gk3mt-^$xei@s*5%#bmm@f^x@8q7u*y)wqs30^0ld%a3^hp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
